9d028eec2bd4a59a22f8d4bf4290d1f2ee10f9a3
DenysPostolnyi <postlden44@gmail.com>
2024-02-13 09:52:31 +0200

    implemented editing user

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,11 +8,29 @@ from django.contrib.auth.admin import (
 
 from core import models
 
+from django.utils.translation import gettext_lazy as _
+
 
 class UserAdmin(BaseUserAdmin):
     """Define admin pages for users"""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)

dfb9036dc4bdf31fe0d95113cacb4eef6f483726
DenysPostolnyi <postlden44@gmail.com>
2024-02-14 10:54:18 +0200

    implemented api for generate token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -62,13 +62,13 @@ class PublicUserApiTests(TestCase):
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
-        """Test generates token for valid credentials."""
+        """Test generates token for valid credentials"""
         user_details = {
             'name': 'Test Name',
             'email': 'test@example.com',
             'password': 'test-user-password123',
         }
-        get_user_model().objects.create(**user_details)
+        get_user_model().objects.create_user(**user_details)
 
         payload = {
             'email': user_details['email'],
@@ -80,7 +80,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_credentials(self):
-        """Test returns error if credentials invalid."""
+        """Test returns error if credentials invalid"""
         get_user_model().objects.create(
             email='test@example.com',
             password='goodpass',
@@ -93,7 +93,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_email_not_found(self):
-        """Test error returned if user not found for given email."""
+        """Test error returned if user not found for given email"""
         payload = {'email': 'test@example.com', 'password': 'pass123'}
         res = self.client.post(TOKEN_URL, payload)
 
@@ -101,7 +101,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
-        """Test posting a blank password returns an error."""
+        """Test posting a blank password returns an error"""
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 

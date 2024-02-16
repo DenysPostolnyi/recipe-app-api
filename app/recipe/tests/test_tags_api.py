c37cc5f938fa6db1c3182c8b481b577936b47607
DenysPostolnyi <postlden44@gmail.com>
2024-02-16 12:28:01 +0200

    optimized imports

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,8 +1,6 @@
 """
 Tests for the tags API
 """
-from decimal import Decimal
-
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -10,9 +8,8 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag, Recipe
+from core.models import Tag
 from recipe.serializers import TagSerializer
-from recipe.tests.test_recipe_api import RECIPES_URL, create_recipe
 
 TAGS_URL = reverse('recipe:tag-list')
 

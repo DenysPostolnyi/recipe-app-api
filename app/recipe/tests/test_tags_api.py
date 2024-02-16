3035d6bb8c0b7a644427ab10c6fd2012d4c88541
DenysPostolnyi <postlden44@gmail.com>
2024-02-16 10:49:56 +0200

    implemented tags api

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -9,6 +9,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Tag
+from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
 

a978f933b28a1400b3d1ec75b9c079e8c5653e65
DenysPostolnyi <postlden44@gmail.com>
2024-02-18 15:13:51 +0200

    small swagger settings

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -105,3 +105,4 @@ class RecipeImageSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'image']
         read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
